45fcddada5cd04bc711e5d9cf8cd09e2eb97bac3
Ray suh <readingod@naver.com>
2021-04-29 12:22:54 +0900

    #27 feat::hamster: Add Network FetchData code

---
+++ iOS/sideDish/Model/SideDish.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+struct SideDish: Decodable {
+    let detailHash: String
+    let image: String
+    let alt: String
+    let deliveryType: [String]
+    let title: String
+    let detail: String
+    let nPrice: String?
+    let sPrice: String
+    let badge: [String]?
+    
+    enum CodingKeys: String, CodingKey {
+        case detailHash = "detail_hash"
+        case image
+        case alt
+        case deliveryType = "delivery_type"
+        case title
+        case detail = "description"
+        case nPrice = "n_price"
+        case sPrice = "s_price"
+        case badge
+    }
+}
--- iOS/sideDish/Service/NetworkManager.swift
+++ iOS/sideDish/Service/NetworkManager.swift
@@ -6,8 +6,13 @@ class NetworkManager {
     static func getSideDishesInfo(_ url: String) {
         AF.request(url)
             .validate(statusCode: 200..<300)
-            .responseDecodable(of: ) { response in
-                
+            .responseDecodable(of: SideDish.self) { response in
+                switch response.result {
+                case .success(let data):
+                    print(data)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
             }
     }
 }
